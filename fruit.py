40dd6d7576d03bb609c44386cdafb4e6703e267d
itamarlevi <itamar@apiiro.com>
2024-07-22 21:55:28 +0300

    test

---
+++ fruit.py
@@ -0,0 +1,20 @@
+from ray import serve
+from ray.serve.handle import DeploymentHandle
+
+# These imports are used only for type hints:
+from typing import Dict
+from starlette.requests import Request
+
+
+@serve.deployment(num_replicas=2)
+class FruitMarket:
+    def __init__(self):
+        print("hello")
+
+
+    async def __call__(self, request: Request) -> float:
+        #fruit, amount = await request.json()
+        #return await self.check_price(fruit, amount)
+        return "hello"
+
+deployment_graph = FruitMarket.bind()
