ccfd061de2c52233591ac73bffeae14c7ad6206f
itamarlevi <itamar@apiiro.com>
2024-07-23 00:43:25 +0300

    test

--- fruit.py
+++ fruit.py
@@ -1,7 +1,7 @@
 from ray import serve
 from ray.serve.handle import DeploymentHandle
-from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
-from google.cloud import storage
+#from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
+#from google.cloud import storage
 from starlette.requests import Request
 import logging
 
